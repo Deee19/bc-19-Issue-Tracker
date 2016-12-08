a92969e556b4b97af6028e9a6115591ac513e313
unknown <damilola.durodola@ymail.com>
2016-12-08 13:11:33 +0100

    Implemented Social Auth

--- public/js/login.js
+++ public/js/login.js
@@ -10,15 +10,44 @@ function login() {
             var errorMessage = error.message;
             alert(errorMessage);
         });
-
 }
 
 function logOut() {
-    firebase.auth().signOut().then(function () {
-        window.location = "index";
-    }, function (error) {
-        // An error happened.
+    firebase.auth().signOut().
+    then(function () {
+            window.location = "/";
+        })
+        .catch(function (error) {
+            // An error happened.
+            var errorMessage = error.message;
+            alert(errorMessage);
+        });
+}
+
+function twitterSign() {
+    var provider = new firebase.auth.TwitterAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+    .then(function (result) {
+        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+        // You can use these server side with your app's credentials to access the Twitter API.
+        var token = result.credential.accessToken;
+        var secret = result.credential.secret;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+        window.location = "main";
+
+    })
+    .catch(function (error) {
+        // Handle Errors here.
+        //var errorCode = error.code;
         var errorMessage = error.message;
-        alert(errorMessage);
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        alert(error);
     });
 }
